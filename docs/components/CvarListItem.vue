0aaf6365268b119bb58600c3e4441f45018f6acc
Viktor Persson <viktor.persson@arcsin.se>
2022-11-07 18:10:40 +0100

    Add read only flag.

--- docs/components/CvarListItem.vue
+++ docs/components/CvarListItem.vue
@@ -8,6 +8,6 @@ export default {
 </script>
 
 <template>
-  <strong>{{ cvar.name }}</strong> <code>default: "{{ cvar.default_value }}"</code>
+  <strong>{{ cvar.name }}</strong> <span v-if="cvar.flags.includes('CVAR_ROM')">(read-only)</span> <code>default: "{{ cvar.default_value }}"</code>
   <div v-if="cvar.description">{{ cvar.description }}</div>
 </template>
